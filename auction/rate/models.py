0d9be1f3d444b0e4cba9b2d4523b3f8324eadbf9
Ramil Latypov <yazuk85@gmail.com>
2018-06-15 13:24:01 +0400

    changes

--- auction/rate/models.py
+++ auction/rate/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -7,11 +7,25 @@ class Lot(models.Model):
 
     class Meta:
         ordering = ['name']
+        verbose_name = "Лот"
+        verbose_name_plural = "Лоты"
 
     name = models.CharField(max_length=200, help_text='Name')
     description = models.CharField(max_length=200)
     status = models.CharField(max_length=200, choices=(('OPEN', 'open'), ('CANCELED', 'canceled'), ('CLOSED', 'closed')))
     pub_date = models.DateTimeField('date published')
+
+
+class Rate(models.Model):
+
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = "Ставка"
+        verbose_name_plural = "Ставки"
+
+    lot = models.ForeignKey(Lot, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
 
 
 
