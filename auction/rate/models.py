342254659a3798cf942a8ae2181cba6d965b7b22
Ramil Latypov <yazuk85@gmail.com>
2018-06-15 12:29:18 +0400

    add model Lot

--- auction/rate/models.py
+++ auction/rate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lot(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, choices=(('OPEN', 'open'), ('CANCELED', 'canceled'), ('CLOSED', 'closed')))
+    pub_date = models.DateTimeField('date published')
+
+
+
+
