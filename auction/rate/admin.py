d668438c9be41461c3bc3c04df56635f4e894399
Ramil Latypov <yazuk85@gmail.com>
2018-06-15 14:46:57 +0400

    changes

--- auction/rate/admin.py
+++ auction/rate/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Lot, Rate
+from .models import Lot, Rate, WinnerLot
 
 # Register your models here.
 admin.site.register(Lot)
 admin.site.register(Rate)
+admin.site.register(WinnerLot)
