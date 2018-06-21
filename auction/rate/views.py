966dfe4ef3177a3e3e4664ce981148f4136fd698
Ramil Latypov <yazuk85@gmail.com>
2018-06-21 17:54:55 +0400

    filter name_lot

--- auction/rate/views.py
+++ auction/rate/views.py
@@ -6,7 +6,7 @@ from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from .serializers import LotSerializer, RateSerializer
 from .models import Lot, Rate, WinnerLot
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -31,9 +31,12 @@ class CreateRate(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         post_request = serializer.save()
-        # users = Rate.user.objects.all()
+        users = User.objects.all()
         name_lot = post_request.get('lot').get('name')
+        name_user = post_request.get('user').get('username')
         lots = Lot.objects.all().filter(name=name_lot)
+        # users = User.objects.all().filter(username=name_user)
+        print(users)
         if len(lots) > 0:
             print(lots)
 
