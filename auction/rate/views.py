83d2001ef59d53983fe01f0a765d69b374f7e5f7
Ramil Latypov <yazuk85@gmail.com>
2018-06-25 11:00:06 +0400

    changes

--- auction/rate/views.py
+++ auction/rate/views.py
@@ -24,7 +24,7 @@ class LotActiveList(generics.ListCreateAPIView):
     model = Lot
     serializer_class = LotSerializer
     queryset = Lot.objects.filter(status="OPEN")
-
+    permission_classes = [permissions.IsAuthenticated]
 
 class CreateRate(generics.ListCreateAPIView):
     """
@@ -36,32 +36,10 @@ class CreateRate(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         # serializer.save()
-
-        user_auth = None
-        lot_request = None
-
-        # защита 80 левела
-        users = User.objects.filter(username=self.request.user)
-        if len(users) == 1:
-            user_auth = users[0]
-
-        # защита 80 левела
-        lots = Lot.objects.filter(name=self.request.data['lot.name'])
-        if len(lots) == 1:
-            lot_request = lots[0]
+        serializer.save(user=self.request.user)
         price_request = self.request.data['price']
-
-        if (user_auth != None) and (lot_request != None):
-            print(user_auth, lot_request, price_request)
-            Rate.objects.create(user=user_auth, lot=lot_request, price=price_request)
-            # return JsonResponse(serializer.data, status=201)
-        else:
-            return JsonResponse(serializer.errors, status=400)
-
-
-
-
-
-
-
-
+        user_auth = User.objects.filter(username=self.request.user)
+        lot_request = Lot.objects.filter(name=self.request.data['lot.name'])
+        print(len(user_auth), len(lot_request))
+        if( (len(user_auth)==1) and (len(lot_request)==1)):
+            Rate.objects.create(user=user_auth[0], lot=lot_request[0], price=price_request)
